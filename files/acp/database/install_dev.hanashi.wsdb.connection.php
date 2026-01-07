1f2304ee1c5cd0bc040bb45f018371c8a1ac9ae9
Peter Lohse <hanashi@hanashi.eu>
2026-01-07 18:33:53 +0100

    first commit

---
+++ files/acp/database/install_dev.hanashi.wsdb.connection.php
@@ -0,0 +1,48 @@
+<?php
+
+use wcf\system\database\table\column\NotNullInt10DatabaseTableColumn;
+use wcf\system\database\table\column\ObjectIdDatabaseTableColumn;
+use wcf\system\database\table\DatabaseTable;
+use wcf\system\database\table\index\DatabaseTableForeignKey;
+use wcf\system\database\table\index\DatabaseTableIndex;
+use wcf\system\database\table\index\DatabaseTablePrimaryIndex;
+
+return [
+    DatabaseTable::create('wcf1_wsdb_record_connection')
+        ->columns([
+            ObjectIdDatabaseTableColumn::create('connectionID'),
+            NotNullInt10DatabaseTableColumn::create('databaseID'),
+            NotNullInt10DatabaseTableColumn::create('recordID'),
+            NotNullInt10DatabaseTableColumn::create('referencedDatabaseID'),
+            NotNullInt10DatabaseTableColumn::create('referencedRecordID'),
+        ])
+        ->indices([
+            DatabaseTablePrimaryIndex::create()
+                ->columns(['connectionID']),
+            DatabaseTableIndex::create('databaseReference')
+                ->columns(['databaseID', 'recordID', 'referencedDatabaseID', 'referencedRecordID'])
+                ->type(DatabaseTableIndex::UNIQUE_TYPE),
+        ])
+        ->foreignKeys([
+            DatabaseTableForeignKey::create()
+                ->columns(['databaseID'])
+                ->referencedTable('wcf1_wsdb_database')
+                ->referencedColumns(['databaseID'])
+                ->onDelete('CASCADE'),
+            DatabaseTableForeignKey::create()
+                ->columns(['referencedDatabaseID'])
+                ->referencedTable('wcf1_wsdb_database')
+                ->referencedColumns(['databaseID'])
+                ->onDelete('CASCADE'),
+            DatabaseTableForeignKey::create()
+                ->columns(['recordID'])
+                ->referencedTable('wcf1_wsdb_record')
+                ->referencedColumns(['recordID'])
+                ->onDelete('CASCADE'),
+            DatabaseTableForeignKey::create()
+                ->columns(['referencedRecordID'])
+                ->referencedTable('wcf1_wsdb_record')
+                ->referencedColumns(['recordID'])
+                ->onDelete('CASCADE'),
+        ]),
+];
