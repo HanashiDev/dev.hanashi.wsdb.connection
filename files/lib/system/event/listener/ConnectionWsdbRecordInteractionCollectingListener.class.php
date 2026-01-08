8717d12c5692cbc8d4ff89c6bd6b48a3374cb01d
Peter Lohse <hanashi@hanashi.eu>
2026-01-08 11:30:50 +0100

    add button and change language

--- files/lib/system/event/listener/ConnectionWsdbRecordInteractionCollectingListener.class.php
+++ files/lib/system/event/listener/ConnectionWsdbRecordInteractionCollectingListener.class.php
@@ -5,7 +5,9 @@ namespace wcf\system\event\listener;
 use wcf\data\DatabaseObject;
 use wcf\data\wsdb\record\Record;
 use wcf\event\wsdb\interaction\user\RecordInteractionCollecting;
+use wcf\form\WsdbConnectionAddForm;
 use wcf\system\interaction\AbstractInteraction;
+use wcf\system\request\LinkHandler;
 use wcf\system\WCF;
 use wcf\util\StringUtil;
 
@@ -24,6 +26,27 @@ final class ConnectionWsdbRecordInteractionCollectingListener
                         '<a href="%s">%s</a>',
                         StringUtil::encodeHTML($object->getEditFormLink()),
                         WCF::getLanguage()->get('wsdb.record.connections')
+                    );
+                }
+            },
+            'edit'
+        );
+        $event->provider->addInteractionBefore(
+            new class('connectionAdd', static fn (Record $record) => $record->canEdit()) extends AbstractInteraction {
+                #[\Override]
+                public function render(DatabaseObject $object): string
+                {
+                    \assert($object instanceof Record);
+
+                    return \sprintf(
+                        '<a href="%s">%s</a>',
+                        StringUtil::encodeHTML(
+                            LinkHandler::getInstance()->getControllerLink(WsdbConnectionAddForm::class, [
+                                '__database' => $object->getDatabase()->path,
+                                'object' => $object,
+                            ])
+                        ),
+                        WCF::getLanguage()->get('wsdb.record.connection.add')
                     );
                 }
             },
